81b1885425da1a4bc5ad59139423b06e4dea4c71
Aiden <bokjaeoh@gmail.com>
2023-07-28 19:45:44 +0900

    Add test tables for testing

--- test/db/initdb.d/createDatabase.sql
+++ test/db/initdb.d/createDatabase.sql
@@ -6,3 +6,38 @@ CREATE USER IF NOT EXISTS 'test'@'localhost' IDENTIFIED BY 'test';
 GRANT ALL PRIVILEGES ON test.* TO 'test'@'%';
 GRANT ALL PRIVILEGES ON test.* TO 'test'@'localhost';
 FLUSH PRIVILEGES;
+
+USE test;
+
+CREATE TABLE IF NOT EXISTS plays (
+    name VARCHAR(16),
+    plays INT,
+    wins INT
+);
+INSERT INTO plays VALUES 
+    ("Jonh", 20, 5),
+    ("Robert", 22, 8),
+    ("Wanda", 32, 8),
+    ("Susan", 17, 3);
+
+CREATE TABLE IF NOT EXISTS a (
+    a_no INT,
+    a_country VARCHAR(1)
+);
+INSERT INTO a VALUES
+    (1, "A"),
+    (2, "B"),
+    (3, "C"),
+    (4, "D"),
+    (5, "E");
+
+CREATE TABLE IF NOT EXISTS b (
+    b_no INT,
+    a_no INT,
+    b_city VARCHAR(2)
+);
+INSERT INTO b VALUES
+    (1, 1, "AA"),
+    (2, 2, "BB"),
+    (3, 6, "FF"),
+    (4, 7, "GG");
