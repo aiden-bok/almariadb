e9d7ec6bf7ff81e39cbddeb75a68b5cd63d2e72d
Aiden <bokjaeoh@gmail.com>
2023-08-03 14:30:09 +0900

    Add test for `queryWithParam` method and tests for methods related to selection

--- test/selectByPool.test.js
+++ test/selectByPool.test.js
@@ -11,10 +11,6 @@ describe('MariaDB select query tests using connection pool', () => {
   }
   almariadb.createPool(config)
 
-  const table = 'plays'
-  const columns = ['name', 'plays']
-  const where = { name: `"John"` }
-
   test(`select() throws error - Promise`, async () => {
     let error, result
 
@@ -54,6 +50,10 @@ describe('MariaDB select query tests using connection pool', () => {
   test(`select() returns result - Promise`, async () => {
     let error, result
 
+    const table = 'plays'
+    const columns = ['name', 'plays']
+    const where = { name: `"John"` }
+
     await almariadb
       .select(table, columns, where)
       .then((res) => {
@@ -71,12 +71,123 @@ describe('MariaDB select query tests using connection pool', () => {
   test(`select() returns result - Await`, async () => {
     let error, result
 
+    const table = 'plays'
+    const columns = ['name', 'plays']
+    const where = { name: `"John"` }
+
     try {
       result = await almariadb.select(table, columns, where)
     } catch (err) {
       error = err
     } finally {
       expect(result).toStrictEqual([{ name: 'John', plays: 20 }])
+      expect(error).toBe(undefined)
+    }
+  })
+
+  test(`selectGroup() returns result - Await`, async () => {
+    let error, result
+
+    const table = 'plays'
+    const columns = '(wins / plays) AS winavg'
+    const where = { name: `"John"` }
+    const group = 'winavg'
+
+    try {
+      result = await almariadb.selectGroup(table, columns, where, group)
+    } catch (err) {
+      error = err
+    } finally {
+      expect(result).toStrictEqual([{ winavg: '0.3000' }])
+      expect(error).toBe(undefined)
+    }
+  })
+
+  test(`selectGroup() returns result(having) - Await`, async () => {
+    let error, result
+
+    const table = 'plays'
+    const columns = '(wins / plays) AS winavg, AVG(plays)'
+    const where = null
+    const group = 'winavg'
+    const having = 'AVG(plays) > 20'
+
+    try {
+      result = await almariadb.selectGroup(table, columns, where, group, having)
+    } catch (err) {
+      error = err
+    } finally {
+      expect(result).toStrictEqual([
+        {
+          'AVG(plays)': '32.0000',
+          winavg: '0.2500'
+        },
+        {
+          'AVG(plays)': '22.0000',
+          winavg: '0.3636'
+        }
+      ])
+      expect(error).toBe(undefined)
+    }
+  })
+
+  test(`selectJoin() returns result - Await`, async () => {
+    let error, result
+
+    const table = 'a AS A'
+    const type = 'INNER'
+    const join = 'b AS B'
+    const on = 'A.a_no = B.a_no'
+    const columns = ['A.a_country', 'B.b_city']
+
+    try {
+      result = await almariadb.selectJoin(table, type, join, on, columns)
+    } catch (err) {
+      error = err
+    } finally {
+      expect(result).toStrictEqual([
+        {
+          a_country: 'A',
+          b_city: 'AA'
+        },
+        {
+          a_country: 'B',
+          b_city: 'BB'
+        }
+      ])
+      expect(error).toBe(undefined)
+    }
+  })
+
+  test(`selectJoinGroup() returns result - Await`, async () => {
+    let error, result
+
+    const table = 'a AS A'
+    const type = 'LEFT'
+    const join = 'b AS B'
+    const on = 'A.a_no = B.a_no'
+    const columns = ['A.a_country', 'B.b_city']
+    const where = null
+    const group = 'B.b_city'
+
+    try {
+      result = await almariadb.selectJoinGroup(
+        table,
+        type,
+        join,
+        on,
+        columns,
+        where,
+        group
+      )
+    } catch (err) {
+      error = err
+    } finally {
+      expect(result).toStrictEqual([
+        { a_country: 'C', b_city: null },
+        { a_country: 'A', b_city: 'AA' },
+        { a_country: 'B', b_city: 'BB' }
+      ])
       expect(error).toBe(undefined)
     }
   })
